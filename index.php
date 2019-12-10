d9a5e5228a211baa9925b711ff1ca9cc9f1b059b
jane <jane100743@g.ncu.edu.tw>
2019-12-11 00:01:56 +0800

    修改index

--- index.php
+++ index.php
@@ -17,15 +17,75 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <!-- bootstrap cdn -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script src="https://kit.fontawesome.com/0920bde990.js" crossorigin="anonymous"></script>
         <!-- title name-->
         <title>說說 TalkTalk</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="index.css">
     </head>
 
     <body class="background text-type">
         <!-- <div class="container"> -->
-        <div class="banner">
+        <!-- 上方nav -->
+        <nav class="navbar sticky-top navbar-expand-lg navbar-dark">
+                <button class="navbar-toggler" id="button01" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                </button>
+                <button class="navbar-toggler" id="button02" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="navleft collapse navbar-collapse" id="navbarTogglerDemo03">
+                    <form class="form-inline" style="margin-bottom: 0px;">
+                            <div class="dropdown">
+                                <a class="dropdown-toggle dropbtn" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Language</a>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                    <a class="dropdown-item" href="#">English</a>
+                                    <a class="dropdown-item" href="#">繁體中文</a>
+                                </div>
+                            </div>
+                            <div class="search">
+                                <span class="fas fa-search" style="color:#707070;padding-left: 15px; padding-right:5px;font-size: 22px"></span>
+                                <input type="text" style="border-color: #707070"> 
+                            </div>
+                        
+                    </form>
+                </div>
+                <a class="navbar-brand mx-auto" href="index.php"> 
+                    <img src="img/logo.png" alt="">
+                    <h4>說說</h4></a>
+                
+                <div class="navright collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <form class="form-inline">           
+                        <img src="img/coin.png"style="padding: 12px">
+                        <?php
+                            if (isset($currentUserPoint)){
+                                echo '<button type="button" class="btn btn-outline-light" style="border-radius: 20pt;">$ '.$currentUserPoint.'</button>';
+                            } else {
+                                echo '<button type="button" class="btn btn-outline-light" style="border-radius: 20pt;">$ ???</button>';
+                            }
+                        ?>
+                        <img src="img/vote_noti.png"  style="padding: 12px; height:300%;">
+                        <div class="dropdown">
+                            <img src="img/vote_user.png" class="dropdown-toggle" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"style="padding: 12px; height:56px; cursor: pointer;">
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuLink">
+                                <?php
+                                    if (isset($currentUser)){
+                                        echo '<a class="dropdown-item" href="#">'.$currentUser.'</a>';
+                                        echo '<a class="dropdown-item" href="history.php">下注紀錄</a>';
+                                        echo '<a class="dropdown-item" href="login.php?status=logout">登出</a>';
+                                    } else {
+                                        echo '<a class="dropdown-item" href="login.php">登入</a>';
+                                    }
+                                ?>
+                            </div>
+                        </div>
+                    </form> 
+                </div>           
+        </nav>
+        <!-- <div class="banner">
             <div class="row upper-nav" style="align-items: center;">
                 <div class="col-sm-1.8">
                     <div class="dropdown">
@@ -69,17 +129,17 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
         <!-- 輪播圖片 -->
         <div class="row">
             <div id="carousel" class="carousel slide" data-interval="false" data-ride="carousel">
                 <ol class="carousel-indicators">
                     <li data-target="#carousel" data-slide-to="0" class="active"></li>
-                    <!-- <li data-target="#carousel" data-slide-to="1"></li>
+                    <li data-target="#carousel" data-slide-to="1"></li>
                     <li data-target="#carousel" data-slide-to="2"></li>
-                    <li data-target="#carousel" data-slide-to="3"></li> -->
+                    <li data-target="#carousel" data-slide-to="3"></li>
                 </ol>
-                <div class="carousel-inner">
+                <div class="carousel-inner" style="data-interval:false">
                     <div class="carousel-item top_pic active">
                         <img class="d-block w-100" src="img/mvp_bg.jpg" alt="First slide">
                         <div class="carousel-caption d-none d-md-block"></div>
@@ -106,6 +166,14 @@
                         </div>
                     </div> -->
                 </div>
+                <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev" style="color: black;">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
             </div>
         </div>
         <!-- 各類型圖示 -->
@@ -390,15 +458,6 @@
                 </div>
             </div>
         </footer>
-
-        <!-- bootstrap cdn -->
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-        <!-- icon cdn-->
-        <!-- <script src="https://kit.fontawesome.com/0920bde990.js" crossorigin="anonymous"></script> -->
-
     </body>
     
 </html>
